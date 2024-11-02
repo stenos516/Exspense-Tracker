23065431a481ccf8919cd0af4e16fa14c191cd95
Stefano.L <83345164+stenos516@users.noreply.github.com>
2024-11-02 13:10:06 +0100

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,54 @@
-import React from "react";
-import { Provider, useSelector } from "react-redux";
+import React, { useState } from "react";
+import { Provider } from "react-redux";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import store from "./app/store";
 import Navbar from "./components/Navbar";
 import AddExpenseForm from "./features/AddExpenseForm";
 import ExpensesList from "./features/expenses/ExpensesList";
 import Balance from "./components/Balance";
-
+import {jwtDecode} from "jwt-decode";
 function App() {
 
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLoginSuccess = (credentialResponse) => {
+    const userObject = jwtDecode(credentialResponse.credential); 
+    const nickname = userObject.given_name;
+    localStorage.setItem('nickname', nickname);
+
+    setIsAuthenticated(true);
+    console.log("Token di Google ricevuto:", credentialResponse.credential);
+  };
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    localStorage.removeItem('expenses');
+  localStorage.removeItem('income');
+  localStorage.removeItem('nickname');
+  }; 
+
   return (
-    <Provider store={store}>
-      <Navbar />
-      <div className="container">
-      <Balance />
-        <AddExpenseForm />
-        <ExpensesList />
-      </div>
-    </Provider>
+    <GoogleOAuthProvider clientId="918292404870-p4snabp9r1iir3kpgcombb9mrd72v6al.apps.googleusercontent.com">
+      <Provider store={store}>
+        <Navbar />
+        <div className="container">
+          {!isAuthenticated ? (
+            <div>
+              <h2>Login for access</h2>
+              <GoogleLogin onSuccess={handleLoginSuccess} onError={() => console.log("Login failed")} />
+            </div>
+          ) : (
+            <div>
+              <button onClick={handleLogout}>Logout</button>
+              <h2>Welcome, {localStorage.getItem('nickname')}!</h2> 
+              <Balance />
+              <AddExpenseForm />
+              <ExpensesList />
+            </div>
+          )}
+        </div>
+      </Provider>
+    </GoogleOAuthProvider>
   );
 }
 
