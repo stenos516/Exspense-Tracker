9598f4317029267dc2dd1e957cf9d9817508552f
Stefano.L <83345164+stenos516@users.noreply.github.com>
2024-11-02 13:11:23 +0100

    Update expensesSlice.js

--- app/src/features/expenses/expensesSlice.js
+++ app/src/features/expenses/expensesSlice.js
@@ -8,13 +8,20 @@ const expensesSlice = createSlice({
   reducers: {
     addExpense: (state, action) => {
       state.expenses.push(action.payload);
+      localStorage.setItem('expenses', JSON.stringify(state.expenses));
     },
     removeExpense: (state, action) => {
       state.expenses = state.expenses.filter(expense => expense.id !== action.payload);
+      localStorage.setItem('expenses', JSON.stringify(state.expenses));
     },
   },
+
+  
+  
+
 });
 
 export const { addExpense, removeExpense } = expensesSlice.actions;
 export default expensesSlice.reducer;
+
 
